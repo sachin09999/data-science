ac64f67eefc7132e651311f65eb625eaa6b4528d
sachin09999 <sachinrajput1362@gmail.com>
2024-05-30 11:24:40 +0530

    hi

--- 5.py
+++ 5.py
@@ -33,16 +33,79 @@
 #     print("the code will run")            
 
 #oops
-class animal:
-    name="elephant"
-    weight=2000
-    age=25
+# class animal:
+#     name="elephant"
+#     weight=2000
+#     age=25
 
-    def display(self):
-      print(self.name)
-      print(self.weight)
-      print(self.age)
+#     def display(self):
+#       print(self.name)
+#       print(self.weight)
+#       print(self.age)
 
+# a=animal() 
+# print(a.age)
 
-a=animal() 
-print(a.age)
+# a.age="999"
+# print(a.age)
+
+# constructor
+    # __init__()
+
+# class animal:
+#     name="Lion"
+#     weight=85
+#     age=100
+    
+#     def __init__(self,name,weight,age):
+#         self.name=name
+#         self.weight=weight
+#         self.age=age
+#         print(self.name,self.weight,self.age)
+#         # print(self.name,self.weight,self.age)
+
+#     # def __init__(self):
+#     #     print(self.name)
+#     #     print(self.weight)
+#     #     print(self.age)
+
+# # a=animal()   
+# b=animal(name="Tiger",weight=400,age=32)     
+
+# class bank:
+#     name
+#     # age=111
+#     # address="alwar"
+#     # bank="swiss bank"
+#     # accno=111222333
+
+#     def show_info(self):
+#         print(self.name)
+#         # print(self.age)
+#         # print(self.address)
+#         # print(self.bank)
+#         # print(self.accno)
+
+#     def get_info(self,name):
+#         self.name=name=input("enter the name")   
+
+# # b=bank()
+# # b.show_info()
+# c=bank()
+# c.get_info()
+# c.show_info()
+
+#inheritance
+class first:
+    sirname="singh"
+    def a(self,n):
+     self.n=sirname
+     
+    # def __init__(self,sirname):
+    #     self.sirname=sirname
+
+class second(first):
+    name="sachin"  
+
+s=second()     
+print(s.name,s.sirname)
