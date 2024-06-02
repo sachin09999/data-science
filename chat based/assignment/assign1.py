5eb2f380b7d8ebe82f2aa152f121d87609592ded
sachin09999 <sachinrajput1362@gmail.com>
2024-06-02 21:24:51 +0530

    assign

---
+++ chat based/assignment/assign1.py
@@ -0,0 +1,23 @@
+#concatenating two strings
+
+a="hello"
+b=" world"
+print(a+b)
+
+#lowercase
+c="SACHIN"
+print(c.lower())
+
+#string len
+print(len(c))
+
+#removing whitespace from string
+st="      helloLIET     "
+print(st.lstrip())
+
+#difference between insert() and append
+lst1=[1,2,3,4,5]
+lst1.append(6)
+print(lst1)
+lst1.insert(2,9)
+print(lst1)
