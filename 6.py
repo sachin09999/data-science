ab19dfb85e3cde9c02ca9329f2f49176d66a18ad
sachin09999 <sachinrajput1362@gmail.com>
2024-06-08 10:49:19 +0530

    saturaday

--- 6.py
+++ 6.py
@@ -1,23 +1,3 @@
-class college:
-    collegename="LIET" #public variable
-    address="alwar"
-class faculty(college):
-    __Salary="50k"   #private variable
-    Branch="CS"
-    
-class student(faculty):
-    name="sachin"    
-    sem="4th"
-    
-    
-    def disp(self):
-        print(self.name)
-        print(self.sem)
-        print(self.Branch)
-        print(self.collegename)
-        print(self.address)
-
-    def __int__(self):
-        self.disp()
-
-s=student()
+import pandas as pd
+lp_df=pd.read_csv("laptops.csv")
+print(lp_df)
